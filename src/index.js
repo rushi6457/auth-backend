7e8826d4b63a6a7498659d6b92f31b79a0582f96
rushi6457 <rushiypatil@gmail.com>
2023-01-20 15:59:51 +0530

    login & signup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require("cors")
+const dotenv = require("dotenv").config()
+const connect = require("./config/db")
+const PORT = process.env.PORT;
+const userRoute = require("./routes/userRoutes")
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors({origin:true,credentials:true}))
+
+app.post("/user/signup",userRoute.Signup)
+app.post("/user/login",userRoute.Login)
+app.get("/",(req,res)=>res.send("Hello"))
+
+app.listen(PORT,async()=>{
+    await connect()
+    console.log(`Server started on http://localhost:${PORT}`);
+})
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,118 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const jwt = require("jsonwebtoken")
+const argon = require("argon2")
+const UserModel =require("../models/userModel")
+
+const Signup = async(req,res) =>{
+
+       const {name,email,password,age}= req.body;
+      
+        const hash = await argon.hash(password)
+        const userExists = await UserModel.findOne({email:email})
+      
+        try {
+              if(userExists){
+            res.send({"message" : "User already exists please login to proceed"})
+        }
+        else{
+        const newUser = new UserModel({
+            name,
+            email,
+            password:hash,
+            age
+        })
+
+        await newUser.save()
+        res.send({"message":"User created successfully"}).status(201)
+    }
+        } catch (error) {
+            res.send(error)
+        }
+}
+const findUser = async (data) => {
+  let user = await UserModel.findOne({ ...data });
+  if (user) {
+    return user;
+  } else {
+    return false;
+  }
+};
+
+const validateUser = async (data) => {
+  let { email, password } = data;
+  try {
+    let user = await findUser({ email });
+    
+    if (user) {
+      if (await argon.verify(user.password, password)) {
+        return user;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  } catch (e) {
+    return false;
+  }
+};
+const Login = async (req, res) => {
+  let { email, password } = req.body;
+  let user = await validateUser({ email, password });
+    console.log(user);
+  if (user ) {
+    let token = jwt.sign(
+      { email: user.email, name: user.name, role: user.role },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "2 days",
+      }
+    );
+
+    let refreshToken = jwt.sign(
+      { email: user.email, name: user.name, role: user.role },
+        process.env.REFRESH_KEY,
+      { expiresIn: "7 days" }
+    );
+     res.status(200).send({ "Message": "Login successfull" , token, refreshToken });
+  } else {
+    return res.send({ status: false, messege: "something went wrong" });
+  }
+};
+// const Login = async(req,res) =>{
+//     const {email,password} = req.body;
+//     const findUser = await UserModel.find({email,password})
+//     console.log(findUser);
+  
+//     try {
+
+//         if(!findUser){
+//             res.send({"message":"User not found"})
+//         }
+//       else if(await argon.verify(findUser.password , password)){
+//             let token = jwt.sign(
+//                 {email:findUser.email},
+//                 process.env.SECRET_KEY,
+//                 {
+//                     expiresIn:"2 days"
+//                 }
+//             );
+//             let refreshToken = jwt.sign(
+//                 {email:findUser.email},
+//                 process.env.REFRESH_KEY,
+//                 {
+//                     expiresIn:"7 days"
+//                 }
+//             )
+//             res.send({"token":token,"refreshToken":refreshToken})
+//         }
+//     } catch (error) {
+//         res.send({"message":error})
+//     }
+// }
+
+module.exports = {
+    Signup,
+    Login
+}
