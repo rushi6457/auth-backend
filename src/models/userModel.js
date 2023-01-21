82c09d1ece8e09fec166cd656b547adec1a14491
rushi6457 <rushiypatil@gmail.com>
2023-01-21 14:35:47 +0530

    add

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,6 +4,8 @@ const UserSchema = new mongoose.Schema({
     name:{type:String, required:true},
     email:{type:String, required:true,unique:true},
     password:{type:String},
+},{
+    timestamps:{type:String, required:true}
 })
 const UserModel = mongoose.model("user",UserSchema)
 module.exports = UserModel
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -82,15 +82,15 @@ const Login = async (req, res) => {
 
 const getProfile = async(req,res) =>{
   
-    let token = req.headers["authorization"]
- 
+  let token = req.headers["authorization"]
+
   try {
     
-      let users = await UserModel.find()
-      
+    if(token){
+          let users = await UserModel.find()
+     
         res.send(users)
-    
-   
+    }
     
   } catch (error) {
     res.send({"message":"Something went wrong"})
